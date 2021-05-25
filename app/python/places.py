bf8f9ffe1fb65ffeffabbecde1c1b3d2d0a8a0bc
Scott Robertson <stumpednomore@gmail.com>
2021-05-25 13:06:06 +0800

    have usable collection called pairs with all the right stuff in it for redefining each id in place_dicts with a single value; after this is done, still have to handle the case of non-contiguous duplicates?

--- app/python/places.py
+++ app/python/places.py
@@ -168,9 +168,11 @@ z = "Masada, Israel"
 aaa = "Israel"
 bbb = "USA"
 ccc = "Dupes, Dupes, Dupes"
+ddd = "table 5, McDonalds, Paris, Lamar County, Texas, USA"
+eee = "table 5, McDonalds, Paris, Bear Lake County, Idaho, USA"
 
-
-place_input = u # u # v
+# ADD A THIRD MULTIPLE TO a and b
+place_input = ddd # u # v # ddd # eee
 class ValidatePlace():
 
     def __init__(self, view, place_input):
@@ -334,29 +336,149 @@ class ValidatePlace():
         pairs = []
         for dkt in self.place_dicts:
             pairs.append(dkt["id"])
-        pairs_pairs = []
-        for i in range(len(pairs)-1):            
-            pairs_pairs.append([pairs[i], pairs[i+1]])
-        pair_us_up = []
-        for pair in pairs_pairs:
-            if len(pair[0]) > 1 or len(pair[1]) > 1:
+        # print("line", looky(seeline()).lineno, "pairs", pairs)
+# line 339 pairs [[802, 803], [795, 796, 797], [30, 32, 34, 684, 685, 729, 731, 732, 733, 735, 737, 739, 742, 744, 745, 746, 748, 750, 752, 753, 754, 755], [78], [29], [8]]
+# INSTEAD OF MAKING ANOTHER LIST, IMMED REPLACE ELEMS IN pairs WITH MULTIPLES USED BELOW
+
+        for i in range(len(pairs)-1):
+            # print(pairs[i])
+            if len(pairs[i]) > 1:
+                pairs[i] = [pairs[i], pairs[i+1]]
+        # # print("line", looky(seeline()).lineno, "pairs", pairs)
+# # line 347 pairs [
+# # [[802, 803], [795, 796, 797]], 
+# # [[795, 796, 797], [30, 32, 34, 684, 685, 739, 742, 744, 745, 746, 748, 750, 752, 753, 754, 755]], 
+# # [[30, 32, 34, 684, 685, 729, 731, 732, 733, 745, 746, 748, 750, 752, 753, 754, 755], [78]], 
+# # [78], 
+# # [29], 
+# # [8]]
+        # pairs_pairs = []
+        # for i in range(len(pairs)-1):            
+            # pairs_pairs.append([pairs[i], pairs[i+1]])
+        # # print("line", looky(seeline()).lineno, "pairs_pairs", pairs_pairs)
+# # line 342 pairs_pairs [
+# # [[802, 803], [795, 796, 797]], 
+# # [[795, 796, 797], [30, 32, 34, 684, 685,9, 742, 744, 745, 746, 748, 750, 752, 753, 754, 755]], 
+# # [[30, 32, 34, 684, 685, 729, 731, 732, 733, 746, 748, 750, 752, 753, 754, 755], [78]], 
+# # [[78], [29]], 
+# # [[29], [8]]]
+        p = 0
+        for pair in pairs:
+            # print("line", looky(seeline()).lineno, "pair", pair)
+            if len(pair) == 1:
+                continue
+            elif len(pair[0]) > 1 or len(pair[1]) > 1:
                 all_pairs = [(i, j) for i in pair[0] for j in pair[1]]
                 st = set(all_pairs).intersection(places_places)
-                tup = tuple(st)
-                pair_us_up.append(tup)
-        paired_up = []
-        for w in range(len(pair_us_up) - 1):
-            child = pair_us_up[w]
-            parent = pair_us_up[w + 1]
-            x = pair_up(child, parent)
-            paired_up.extend(x)
-        print("line", looky(seeline()).lineno, "paired_up", paired_up)
-# line 359 paired_up [((796, 30), (30, 78)), ((795, 34), (30, 78))]
-# make sure this works with three multiples in one nesting
+                tup = list(st)
+                pairs[p] = tup
+            p += 1    
+        # print("line", looky(seeline()).lineno, "pairs", pairs)
+# line 376 pairs [
+# ((803, 796), (802, 795)), 
+# ((796, 30), (795, 34)), 
+# ((30, 78),), 
+# [78], 
+# [29], 
+# [8]]
+        # pair_us_up = []
+        # for pair in pairs_pairs:
+            # if len(pair[0]) > 1 or len(pair[1]) > 1:
+                # all_pairs = [(i, j) for i in pair[0] for j in pair[1]]
+                # st = set(all_pairs).intersection(places_places)
+                # tup = tuple(st)
+                # pair_us_up.append(tup)
+            # else:
+                # pair_us_up.append(None)
+        # print("line", looky(seeline()).lineno, "pair_us_up", pair_us_up)
+# line 351 pair_us_up [((803, 796), (802, 795)), ((796, 30), (795, 34)), ((30, 78),)]
+        y = 0
+        for pair in pairs:
+            if len(pair) == 1:
+                continue
+            else:
+                child = pairs[y]
+                parent = pairs[y + 1]
+                x = pair_up(child, parent)
+                print("line", looky(seeline()).lineno, "x", x)
+                pairs[y] = x
+            y += 1    
+  
+        # print("line", looky(seeline()).lineno, "pairs", pairs)
+
+        # paired_up = []
+        # for w in range(len(pair_us_up) - 1):
+            # child = pair_us_up[w]
+            # parent = pair_us_up[w + 1]
+            # x = pair_up(child, parent)
+            # paired_up.extend(x)
+
+        deletables = []
+        for lst in pairs:
+            if len(lst) == 1:
+                deletables.append([])
+            else:
+                bad = []
+                h = 0
+                for pair in lst:
+                    if pair[0][1] != pair[1][0]:
+                        bad.append(h)
+                    h += 1
+                deletables.append(bad)
+
+        # print("line", looky(seeline()).lineno, "deletables", deletables)
+# line 436 deletables [[1, 2], [1], [], [], [], []]
+# line 406 pairs [
+# [((803, 796), (796, 30)), ((803, 796), (795, 34)), ((802, 795), (796, 30)), ((802, 795), (795, 34))], 
+# [((796, 30), (30, 78)), ((795, 34), (30, 78))], 
+# [(30, 78)], 
+# [78], 
+# [29], 
+# [8]]
+        copy = []
+        q = 0
+        for lst in pairs:
+            if len(lst) == 1:
+                copy.append(lst)
+            else:
+                ok = []
+                p = 0
+                for pair in lst:
+                    if p not in deletables[q]:
+                        ok.append(pair)
+                    p += 1
+                copy.append(ok)
+            q += 1
+        pairs = copy
 
 
+        # print("line", looky(seeline()).lineno, "pairs", pairs)
+# line 455 pairs [
+# [((803, 796), (796, 30)), ((802, 795), (795, 34))], 
+# [((796, 30), (30, 78))], 
+# [(30, 78)], 
+# [78], 
+# [29], 
+# [8]]
+        # deletables = []
+        # g = 0
+        # for pair in paired_up:
+            # if pair[0][1] != pair[1][0]:
+                # deletables.append(g)
+            # g += 1
+        # copy = []
+        # p = 0
+        # for pair in paired_up:
+            # if p not in deletables:
+                # copy.append(pair)
+            # p += 1
+        # paired_up = copy
+        # print("line", looky(seeline()).lineno, "paired_up", paired_up)
 
 
+# ABOVE GETS RIGHT ANSWER(S) BUT NEEDS OTHER NESTS TO FILTER DOWN TO ONE RIGHT ANSWER
+# line 369 paired_up [((803, 796), (796, 30)), ((802, 795), (795, 34)), ((796, 30), (30, 78))]
+# not quite right bec the 3 tups are equals, solution is to not put them into paired up but to substitute them into id_nests while in the right index IF they are matched to each other
 
 if __name__ == "__main__":
 
@@ -376,7 +498,13 @@ if __name__ == "__main__":
     
     view.mainloop()
 
-    
+# DO LIST
+
+# test it with non-contiguous multiples eg:
+#   aaa, bbb, ccc, ddd, eee
+#   aaa, xxx, ccc, fff, ggg
+# in which aaa and ccc are not next to each other
+# when entering a single place there will always be a dialog unless the place is new to the db    
 
 
 
